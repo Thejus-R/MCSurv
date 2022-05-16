12136cba7cf94d3032df2d42360382d31d6a9d91
Thejus Rajendran <thejusr1999@gmail.com>
2022-05-17 03:29:28 +0530

    17/5 - Thejus

---
+++ db-service/database.py
@@ -0,0 +1,90 @@
+import json
+from turtle import update
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+
+
+# Initializing Firebase Admin SDK
+cred = credentials.Certificate("cred.json")
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mcsur-a0dd2-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+
+class Challan:
+    def __init__(self, vehicleRegNo, ownerName, timeStamp, camID, dateStamp):
+        self.vehicleRegNo = vehicleRegNo
+        self.ownerName = ownerName
+        self.timeStamp = timeStamp
+        self.camID = camID
+        self.dateStamp = dateStamp
+
+    # Creating a JSON string from Python Object
+    def createJSON(self):
+        return json.dumps(self.__dict__)
+
+    def getChallanNo():
+        ref = db.reference('/lastChallanNo')
+        return ref.get()
+
+    def createChallan(self):
+        challanNo = updateChallanNo()
+        ref = db.reference("".join(["/challanList/", str(challanNo)]))
+        ref.set(self.createJSON())
+
+
+def getChallanNo():
+    ref = db.reference('/lastChallanNo')
+    return ref.get()
+
+
+def updateChallanNo():
+    ref = db.reference('/lastChallanNo')
+    ref.set(getChallanNo()+1)
+    return getChallanNo()
+
+
+x = Challan('KL-02-BD-5008', "Thejus Rajendran",
+            "12:30PM", "CHN01", "02/04/2022")
+x.createChallan()
+
+# def getChallanNo():
+#     ref = db.reference('/lastChallanNo')
+#     return ref.get()
+
+
+# def updateChallanNo():
+#     ref = db.reference('/lastChallanNo')
+#     ref.set(getChallanNo()+1)
+
+
+# print(x.createJSON())
+
+
+# data1 = {"001": {
+#     "regNo": "KL-03-BD-5008",
+#     "time": "12:45PM"
+# }}
+
+
+# data2 = {"002": {
+#     "regNo": "KL-03-BC-5043",
+#     "time": "1:45PM"
+# }}
+
+# # Parse JSON into an object with attributes corresponding to dict keys.
+# ref = db.reference('/billNo')
+# ref.set(data1)
+
+# ref = db.reference('/billNo/001')
+# x = ref.get()
+# print(x['regNo'], x['time'])
+
+# ref = db.reference('/billNo')
+# ref.update(data2)
+
+
+# ref = db.reference('/billNo/002')
+# x = ref.get()
+# print(x['regNo'], x['time'])
---
+++ db-service/main.py
@@ -0,0 +1,2 @@
+challanNo = 1
+print("".join(["/billNo/", str(challanNo)]))
