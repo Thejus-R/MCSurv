4746da9db3428777b43feb36fcf8ec97a66e4bfc
Thejus Rajendran <thejusr1999@gmail.com>
2022-05-19 13:09:25 +0530

    - updated web-server

---
+++ web-server/src/components/searchBar.svelte
@@ -0,0 +1,6 @@
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    
+    <div class="shrink-0">
+      <input type="text" placeholder="Search">
+    </div>
+  </div>
